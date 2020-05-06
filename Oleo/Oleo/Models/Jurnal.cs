09557414045d055f8a01c8d1fc79aa2674644d40
alexandraulcn <uilacan.alexandra@yahoo.ro>
2020-05-06 12:54:29 +0300

    PersonalBlends section added

--- Oleo/Oleo/Models/Jurnal.cs
+++ Oleo/Oleo/Models/Jurnal.cs
@@ -7,17 +7,15 @@ namespace Oleo.Models
 {
     public class Jurnal
     {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
         public string Nume { get; set; }
         public string Ulei1 { get; set; }
         public string Ulei2 { get; set; }
         public string Ulei3 { get; set; }
-        public int Cant1 { get; set; }
-        public int Cant2 { get; set; }
-        public int Cant3 { get; set; }
+        public string Cant1 { get; set; }
+        public string Cant2 { get; set; }
+        public string Cant3 { get; set; }
         public string Carrier { get; set; }
-        public int CantCarrier { get; set; }
+        public string CantCarrier { get; set; }
         public string Utilizare { get; set; }
 
     }
---
+++ Oleo/Oleo/ViewModels/JurnalPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Oleo.Models;
+
+namespace Oleo.ViewModels
+{
+    public class JurnalPageViewModel
+    {
+        public ObservableCollection<Jurnal> JurnalList { get; set; }
+
+        public JurnalPageViewModel()
+        {
+            JurnalList = new ObservableCollection<Jurnal>();
+            JurnalList.Add(new Jurnal
+            {
+                Nume = "Easy Breath",
+                Ulei1 = "peppermint",
+                Cant1 = "3",
+                Ulei2 = "eucalyptus",
+                Cant2= "2",
+                Ulei3="R.C.",
+                Cant3="5",
+                Carrier="ulei de migdale",
+                CantCarrier="8",
+                Utilizare="aplicare in zona plamanilor"
+            });
+        }
+    }
+}
