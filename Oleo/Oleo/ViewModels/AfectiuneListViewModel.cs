5f524dbc502b33f1d9b5737f71b7497548404c06
alexandraulcn <uilacan.alexandra@yahoo.ro>
2020-05-21 18:01:29 +0300

    dill

--- Oleo/Oleo/ViewModels/AfectiuneListViewModel.cs
+++ Oleo/Oleo/ViewModels/AfectiuneListViewModel.cs
@@ -32,32 +32,32 @@ namespace Oleo.ViewModels
             Afectiuni.Add(new Afectiune
             {
                 Denumire = "Astm",
-                Descriere = "In timpul unei crize de astm, caile respiratorii pulmonare se umfla si se infunda cu mucus gros si lipicios. muschii cailor respiratorii incep sa se contracteze sau sa se stranga. Lucrul acesta poate duce la o respiratie foarte groaie. Daca criza este foarte grava, poate fi amenintata chiar viata. Multe crize de astm sunt declansate de o reactie alergica la polen, la particule de praf, par de pisica sau caine, precum si de la alte alimente precum oua, lapte, coloranti sau alte chimicale.Astmul mai poate fi declansat si de infectii respiratorii, stres si factori psihologici.",
+                Descriere = "În timpul unei crize de astm, căile respiratorii pulmonare se umflă și se înfundă cu mucus gros și lipicios. mușchii căilor respiratorii încep să se contracteze sau să se strângă. Lucrul acesta poate duce la o respirație foarte groaie. Dacă criza este foarte gravă, poate fi amenințată chiar viață. Multe crize de astm sunt declanșate de o reacție alergica la polen, la particule de praf, păr de pisică sau câine, precum și de la alte alimente precum ouă, lapte, coloranți sau alte chimicale.Astmul mai poate fi declanșat și de infecții respiratorii, stres și factori psihologici.",
                 Recomandari = "eucalyptus(E. radiata), frankincense, eucalyptus blue, ravintsara, palo santo, R.C",
-                Aplicare = "Pentru inhalare, pulverizati timp de 3-5 minute un ulei la alegere, puneti 2-3 picaturi dintr-un ulei in palme si tineti-le la nas, inhaland pe parcursul zilei la nevoie. Mai puteti aplica extern 1-2 picaturi pe tample si pe spatele gatului, dupa dorinta."
+                Aplicare = "Pentru inhalare, pulverizați timp de 3-5 minute un ulei la alegere, puneți 2-3 picături dintr-un ulei în palme și țineți-le la nas, inhalând pe parcursul zilei la nevoie. Mai puteți aplica extern 1-2 picături pe tâmple și pe spatele gâtului, după dorință."
             });
 
             Afectiuni.Add(new Afectiune
             {
                 Denumire = "Arsuri de gradul I (arsuri solare)",
-                Descriere = "Cea mai eficienta metoda de prevenire a arsurilor solare este evitarea expunerii prelungite la soare.Cand iesiti afara, purtati intotdeanu obiecte care va protejeaza si folositi lotiuni cu factori de protectie mai mare de 15.",
+                Descriere = "Cea mai eficientă metodă de prevenire a arsurilor solare este evitarea expunerii prelungite la soare.Când ieșiți afară, purtați intotdeanu obiecte care vă protejează și folosiți loțiuni cu factori de protecție mai mare de 15.",
                 Recomandari = "spikenard, lavender, helichrysum, rose, german chamomile, melrose, valor",
-                Aplicare = "Pentru o vindecare rapida a arsurilor de gradul I, racoriti zona afectata. Aplicati 1-3 picaturi de ulei simplu sau diluat 50/50 pe locul arsurii pentru a raci tesaturile si a reduce inflamarea. Aplicati de 3-6 ori pe zi sau la nevoie."
+                Aplicare = "Pentru o vindecare rapidă a arsurilor de gradul I, răcoriți zona afectată. Aplicați 1-3 picături de ulei simplu sau diluat 50/50 pe locul arsurii pentru a răci țesăturile și a reduce inflamarea. Aplicați de 3-6 ori pe zi sau la nevoie."
             });
 
             Afectiuni.Add(new Afectiune
             {
                 Denumire = "Arsuri la stomac",
-                Descriere = "Sucul de lamaie este unul dintre cele mai bune remedii pentru arsurile la stomac. Amestecati sucul de la o jumatate de lamaie stoarsa cu un pahar de apa si beti incet dimineata la trezire. Sucul de lamaie ajuta stomacul sa nu mai produca acid, prin urmare atenueaza arsurile sau alte afectiuni ale stomacului.",
+                Descriere = "Sucul de lămâie este unul dintre cele mai bune remedii pentru arsurile la stomac. Amestecați sucul de la o jumătate de lămâie stoarsă cu un pahar de apă și beți încet dimineața la trezire. Sucul de lămâie ajută stomacul să nu mai producă acid, prin urmare atenuează arsurile sau alte afecțiuni ale stomacului.",
                 Recomandari = "spearmint, ginger, lemon, cypress, tarragon, fennel, Di-Gize",
-                Aplicare = "Reteta pentru arsuri la stomac: 2 picaturi basil, 1 picatura Idaho tansy, 8 picaturi sage, 3 picaturi sandalwood. Aplicati o compresa calda pe stomac, 1-3 picaturi de ulei recomandat sau din reteta. Diluati 50/50 si aplicati local de 3-6 pe zi. De asemenea, luati o capsula cu unul dintre uleiurile recomandate, de 2 ori pe zi. "
+                Aplicare = "Rețetă pentru arsuri la stomac: 2 picături basil, 1 picătură Idaho tansy, 8 picături sage, 3 picături sandalwood. Aplicați o compresă caldă pe stomac, 1-3 picături de ulei recomandat sau din rețetă. Diluați 50/50 și aplicați local de 3-6 pe zi. De asemenea, luați o capsulă cu unul dintre uleiurile recomandate, de 2 ori pe zi. "
             });
 
             Afectiuni.Add(new Afectiune
             {
                 Denumire = "Balonare",
-                Descriere= "Balonarea este o tulburare gastrointestinală ce se întâlnește atât la adulți, cât și la copii. În termeni medicali ea poartă numele de meteorism abdominal și este provocat de o masă prea bogată, de consumul de dulciuri și anumite alimente, cât și de tulburări hormonale sau stres. Exista, însă, și cazuri în care acest disconfort poate ascunde boli grave, așa că nu trebuie să o tratați cu indiferentă.",
-                Recomandari="juniper, clary sage, cypress, peppermint, fennel, nutmeg, di-gize",
+                Descriere = "Balonarea este o tulburare gastrointestinală ce se întâlnește atât la adulți, cât și la copii. În termeni medicali ea poartă numele de meteorism abdominal și este provocat de o masă prea bogată, de consumul de dulciuri și anumite alimente, cât și de tulburări hormonale sau stres. Există, însă, și cazuri în care acest disconfort poate ascunde boli grave, așa că nu trebuie să o tratați cu indiferentă.",
+                Recomandari = "juniper, clary sage, cypress, peppermint, fennel, nutmeg, di-gize",
                 Aplicare= "Cantitatea de ulei înghițit depinde de la ulei la ulei. Adăugați câteva picături din uleiurile recomandate într-un lichid și beți zilnic. Luați o capsulă de peppermint de 3 ori pe zi."
             });
 
--- Oleo/Oleo/ViewModels/ExampleListViewModel.cs
+++ Oleo/Oleo/ViewModels/ExampleListViewModel.cs
@@ -14,7 +14,7 @@ namespace Oleo.ViewModels
             Examples = new List<Example>();
             Examples.Add(new Example
             {
-                Nume = "BREATHE EASY",
+                Nume = "RESPIRĂ UȘOR",
                 Ulei1 = "R.C",
                 Cant1 = "4",
                 Ulei2 = "Eucalyptus",
@@ -35,6 +35,64 @@ namespace Oleo.ViewModels
                 Cant3 = "3"
 
             });
+
+            Examples.Add(new Example
+            {
+                Nume = "STARE DE BINE",
+                Ulei1 = "Lavanda",
+                Cant1 = "4",
+                Ulei2 = "Salvie",
+                Cant2 = "3",
+                Ulei3 = "Rozmarin",
+                Cant3 = "3"
+
+            });
+            Examples.Add(new Example
+            {
+                Nume = "ENERGIE",
+                Ulei1 = "Portocală",
+                Cant1 = "3",
+                Ulei2 = "Peppermint",
+                Cant2 = "3",
+                Ulei3 = "Cocos",
+                Cant3 = "2"
+
+            });
+
+            Examples.Add(new Example
+            {
+                Nume = "FOCUS",
+                Ulei1 = "Rozmarin",
+                Cant1 = "3",
+                Ulei2 = "Patchouli",
+                Cant2 = "3",
+                Ulei3 = "Lămâie",
+                Cant3 = "3"
+
+            });
+            Examples.Add(new Example
+            {
+                Nume = "CALM",
+                Ulei1 = "Tămâie",
+                Cant1 = "5",
+                Ulei2 = "Peace & Calming",
+                Cant2 = "5",
+                Ulei3 = "Cocos",
+                Cant3 = "3"
+
+            });
+            Examples.Add(new Example
+            {
+                Nume = "BOOST IMUNITAR",
+                Ulei1 = "Thieves",
+                Cant1 = "5",
+                Ulei2 = "Lămâie",
+                Cant2 = "3",
+                Ulei3 = "Purification",
+                Cant3 = "3"
+
+            });
+
         }
     }
 }
